25209dcc637c7e6e5d9676b388ad220aa09552a1
Michael Siddiqui <siddiqui@zymergen.com>
2017-10-23 00:59:39 +0000

    add initial package module content to the base subdirectory including simple DNA manipulation Python class

---
+++ base/dna_utils.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+dna_utils module to hold simple utility functions/classes
+"""
+
+DNA_ALPHABET = {
+    'A': 'T',
+    'C': 'G',
+    'G': 'C',
+    'T': 'A'
+}
+
+class DnaUtil(object):
+    """
+    Holds DNA sequence string data enables simple sequence methods.
+    Automatically checks that characters provided in the input string
+    are unambiguous DNA characters and casts all strings to uppercase.
+
+    Args:
+        sequence (str): DNA sequence provided
+
+    Attributes:
+        sequence (str): DNA sequence stored
+        DNA_ALPHABET (dict): lookup to validate accepted characters and
+            implement reverse complement calculation
+    """
+    def __init__(self, sequence):
+        self.DNA_ALPHABET = DNA_ALPHABET
+        sequence = str(sequence)
+        sequence = sequence.upper()
+        self.sequence = sequence
+        self.char_check()
+
+
+    def char_check(self):
+        """check that current stored sequence contains only accepted DNA
+        characters
+        """
+        accepted_chars = set(self.DNA_ALPHABET.keys())
+        if not set(array_string).issubset(accepted_chars):
+            raise ValueError(
+                "expect IUPAC non-ambiguous DNA characters in input"
+                )
+
+    def reverse_complement(self):
+        """return a string that is the reverse complement of the stored
+        DNA sequence
+        """
+        reverse_dna = self.sequence[::-1]
+        output = ''.join([
+            self.DNA_ALPHABET[base] for base in reverse_dna
+        ])
+        return output
