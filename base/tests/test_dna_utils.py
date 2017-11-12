49bfed483c2bf27e3056ea7845752328c18e0d09
Michael Siddiqui <siddiqui@alumni.upenn.edu>
2017-11-11 18:47:20 -0800

    add test class for DnaFreqCalc with test methods validating the pattern_count method with fixtures from the course

--- base/tests/test_dna_utils.py
+++ base/tests/test_dna_utils.py
@@ -16,6 +16,8 @@ import os
 import unittest
 
 from base.dna_utils import DnaUtil
+from base.dna_utils import DnaFreqCalc
+from base.file_utils import FileUtil
 from base.tests.fixtures import FIXTURE_PATH
 
 GOOD_DNA = 'ACGTACGT'
@@ -23,6 +25,11 @@ LOWER_DNA = 'acgtacgt'
 BAD_DNA = 'ACGBT2'
 TEST_FWD_SEQ = 'AATTGGCC'
 TEST_REV_COMP_SEQ = 'GGCCAATT'
+
+FIXTURE1_FNAME = 'SamplePatternCount.txt'
+FIXTURE2_FNAME = 'PatternCount.txt'
+FIXTURE1_FPATH = os.path.join(FIXTURE_PATH, FIXTURE1_FNAME)
+FIXTURE2_FPATH = os.path.join(FIXTURE_PATH, FIXTURE2_FNAME)
 
 class TestDnaUtilsMethods(unittest.TestCase):
     """
@@ -65,4 +72,32 @@ class TestDnaUtilsMethods(unittest.TestCase):
         dna_util_obj = DnaUtil(TEST_FWD_SEQ)
         expected = TEST_REV_COMP_SEQ
         actual = dna_util_obj.reverse_complement()
+        self.assertEqual(expected, actual)
+
+class TestDnaFreqCalcMethods(unittest.TestCase):
+    """
+    Test methods of DnaFreqCalc class from course examples
+    """
+    def test_pattern_count_sample(self):
+        """
+        Test the pattern_count method using the SamplePatternCount.txt inputs
+        """
+        file_util_obj = FileUtil(FIXTURE1_FPATH)
+        inputs = file_util_obj.inputs
+        expected = int(file_util_obj.outputs[0])
+        sequence_obj = DnaFreqCalc(inputs[0])
+        actual = sequence_obj.pattern_count(inputs[1])
+        self.assertEqual(expected, actual)
+
+    def test_pattern_count_extra_dataset(self):
+        """
+        Test the pattern_count method using the PatternCount.txt fixture
+
+        The course prompt called this the "Extra Dataset"
+        """
+        file_util_obj = FileUtil(FIXTURE2_FPATH)
+        inputs = file_util_obj.inputs
+        expected = int(file_util_obj.outputs[0])
+        sequence_obj = DnaFreqCalc(inputs[0])
+        actual = sequence_obj.pattern_count(inputs[1])
         self.assertEqual(expected, actual)
