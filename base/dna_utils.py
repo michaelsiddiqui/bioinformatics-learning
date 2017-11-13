9d3734a3e37d001c881ae44d8a695d809cf5e8be
Michael Siddiqui <siddiqui@alumni.upenn.edu>
2017-11-12 20:07:02 -0800

    remove DnaFreqCalc class from dna_utils module and edit for flake8 nits

--- base/dna_utils.py
+++ base/dna_utils.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-dna_utils module to hold simple utility functions/classes
+dna_utils module to hold a simple utility class
 """
 
-from collections import Counter
 
 DNA_ALPHABET = {
     'A': 'T',
@@ -56,49 +55,3 @@ class DnaUtil(object):
             self.DNA_ALPHABET[base] for base in reverse_dna
         ])
         return output
-
-class DnaFreqCalc(DnaUtil):
-    """
-    Holds a DNA sequence. Enables quick calculation of relevant frequency data
-    """
-    def pattern_count(self, pattern):
-        """
-        Initial pattern_count method
-        """
-        count = 0
-        pattern_util_obj = DnaUtil(pattern)
-        pattern_len = len(pattern)
-        diff_length = len(self.sequence) - pattern_len
-        for i in range(diff_length + 1):
-            end = i + pattern_len
-            window_text = self.sequence[i:end]
-            if window_text == pattern:
-                count +=1
-        return count
-
-    def frequent_kmers(self, k):
-        """
-        Initial frequent_kmers methods
-        """
-        diff_length = len(self.sequence) - k
-        kmer_list = []
-        for i in range(diff_length + 1):
-            kmer_list.append(self.sequence[i: i+ k])
-        kmer_frequency = Counter(kmer_list)
-        return kmer_frequency
-
-    def most_frequent_kmers(self, k):
-        """
-        List the most frequent kmers in the object's sequence
-
-        Use the 'frequent_kmers' method to get Counter object
-        use most_common() method and then retun all keys with
-        values equal to the value of the most most_common element
-        """
-        kmer_frequency = self.frequent_kmers(k)
-        most_freq = kmer_frequency.most_common(1)
-        high_count = most_freq[1]
-        list_most_frequent_kmers = [
-            key for key, val in kmer_frequency.iteritems if val == high_count
-        ]
-        return list_most_frequent_kmers
