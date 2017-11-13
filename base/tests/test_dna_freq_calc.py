ce640d7dcb5a60271c6b1e747d52ba93e3026cef
Michael Siddiqui <siddiqui@alumni.upenn.edu>
2017-11-12 21:07:07 -0800

    add more pattern_count and initial most_frequent_kmer test methods

--- base/tests/test_dna_freq_calc.py
+++ base/tests/test_dna_freq_calc.py
@@ -21,15 +21,19 @@ from base.tests.fixtures import FIXTURE_PATH
 
 FIXTURE1_FNAME = 'SamplePatternCount.txt'
 FIXTURE2_FNAME = 'PatternCount.txt'
+FIXTURE3_FNAME = 'SampleFrequentKmers.txt'
+FIXTURE4_FNAME = 'FrequentKmers.txt'
 FIXTURE1_FPATH = os.path.join(FIXTURE_PATH, FIXTURE1_FNAME)
 FIXTURE2_FPATH = os.path.join(FIXTURE_PATH, FIXTURE2_FNAME)
+FIXTURE3_FPATH = os.path.join(FIXTURE_PATH, FIXTURE3_FNAME)
+FIXTURE4_FPATH = os.path.join(FIXTURE_PATH, FIXTURE3_FNAME)
 PATTERN_COUNT_TEST_FNAMES = [
     fname for fname in os.listdir(FIXTURE_PATH) if 'PatternCountTest' in fname
 ]
 PATTERN_COUNT_TEST_FNAMES.sort()
 
 
-class TestDnaFreqCalcMethods(unittest.TestCase):
+class TestDnaFreqCalc(unittest.TestCase):
     """
     Test methods of DnaFreqCalc class from course examples
     """
@@ -107,4 +111,71 @@ class TestDnaFreqCalcMethods(unittest.TestCase):
         expected = int(file_util_obj.outputs[0])
         sequence_obj = DnaFreqCalc(inputs[0])
         actual = sequence_obj.pattern_count(inputs[1])
+        self.assertEqual(expected, actual)
+
+    def test_pattern_count_test_three(self):
+        """
+        Test the pattern_count method using the TEST DATASET 3 fixture
+
+        Saved in PatternCountTest3.txt
+        This dataset checks if your code correctly handles cases where there
+        is an occurrence of Pattern at the very end of Text.
+        Note that there are no overlapping occurrences of Pattern (i.e. AAAA),
+        and there is no occurrence of Pattern at the very beginning of Text,
+        so assuming your code passed Test Dataset 2, this test would only check
+        for off­by­one errors at the end of Text.
+        """
+        fname = PATTERN_COUNT_TEST_FNAMES[2]
+        fpath = os.path.join(FIXTURE_PATH, fname)
+        file_util_obj = FileUtil(fpath)
+        inputs = file_util_obj.inputs
+        expected = int(file_util_obj.outputs[0])
+        sequence_obj = DnaFreqCalc(inputs[0])
+        actual = sequence_obj.pattern_count(inputs[1])
+        self.assertEqual(expected, actual)
+
+    def test_pattern_count_test_four(self):
+        """
+        Test the pattern_count method using the TEST DATASET 3 fixture
+
+        Saved in PatternCountTest3.txt
+        This test dataset checks if your code is also counting occurrences of
+        the Reverse Complement of Pattern (which would have an output of 4),
+        which is out of the scope of this problem (that will come up later in
+        the chapter). Your code should only be looking for perfect matches of
+        Pattern in Text at this point.
+        """
+        fname = PATTERN_COUNT_TEST_FNAMES[3]
+        fpath = os.path.join(FIXTURE_PATH, fname)
+        file_util_obj = FileUtil(fpath)
+        inputs = file_util_obj.inputs
+        expected = int(file_util_obj.outputs[0])
+        sequence_obj = DnaFreqCalc(inputs[0])
+        actual = sequence_obj.pattern_count(inputs[1])
+        self.assertEqual(expected, actual)
+
+    def test_most_frequent_kmer_sample(self):
+        """
+        Test the pattern_count method using the SamplePatternCount.txt inputs
+
+        From prompt: The sample dataset is not actually run on your code.
+        """
+        file_util_obj = FileUtil(FIXTURE3_FPATH)
+        inputs = file_util_obj.inputs
+        expected = file_util_obj.outputs
+        sequence_obj = DnaFreqCalc(inputs[0])
+        actual = sequence_obj.most_frequent_kmers(inputs[1])
+        self.assertEqual(expected, actual)
+
+    def test_most_frequent_kmer_extra_dataset(self):
+        """
+        Test the pattern_count method using the FrequentKmers.txt fixture
+
+        The course prompt called this the "Extra Dataset"
+        """
+        file_util_obj = FileUtil(FIXTURE4_FPATH)
+        inputs = file_util_obj.inputs
+        expected = file_util_obj.outputs
+        sequence_obj = DnaFreqCalc(inputs[0])
+        actual = sequence_obj.most_frequent_kmers(inputs[1])
         self.assertEqual(expected, actual)
