56d95277edac5fb4a69a87d7888b079ba9db3e8f
Michael Siddiqui <siddiqui@alumni.upenn.edu>
2017-11-12 20:31:48 -0800

    add two test methods to TestDnaFreqCalc TestCase class using the two new fixtures

--- base/tests/test_dna_freq_calc.py
+++ base/tests/test_dna_freq_calc.py
@@ -23,6 +23,10 @@ FIXTURE1_FNAME = 'SamplePatternCount.txt'
 FIXTURE2_FNAME = 'PatternCount.txt'
 FIXTURE1_FPATH = os.path.join(FIXTURE_PATH, FIXTURE1_FNAME)
 FIXTURE2_FPATH = os.path.join(FIXTURE_PATH, FIXTURE2_FNAME)
+PATTERN_COUNT_TEST_FNAMES = [
+    fname for fname in os.listdir(FIXTURE_PATH) if 'PatternCountTest' in fname
+]
+PATTERN_COUNT_TEST_FNAMES.sort()
 
 
 class TestDnaFreqCalcMethods(unittest.TestCase):
@@ -32,6 +36,16 @@ class TestDnaFreqCalcMethods(unittest.TestCase):
     def test_pattern_count_sample(self):
         """
         Test the pattern_count method using the SamplePatternCount.txt inputs
+
+        From prompt: The sample dataset is not actually run on your code.
+        Notice that “GCG” occurs twice in Text: once at the beginning (GCG​CG)
+        and once at the end (GCGCG​). A common mistake for this problem is
+        incorrectly handling overlaps and not counting the second of these two
+        occurrences (because it begins at the end of the previous occurrence).
+        The sample dataset checks for the following things:
+        * Off­by­one at the beginning of Text (result would be 1)
+        * Off­by­one at the end of Text (result would be 1)
+        * Not counting overlaps (result would be 1)
         """
         file_util_obj = FileUtil(FIXTURE1_FPATH)
         inputs = file_util_obj.inputs
@@ -47,6 +61,48 @@ class TestDnaFreqCalcMethods(unittest.TestCase):
         The course prompt called this the "Extra Dataset"
         """
         file_util_obj = FileUtil(FIXTURE2_FPATH)
+        inputs = file_util_obj.inputs
+        expected = int(file_util_obj.outputs[0])
+        sequence_obj = DnaFreqCalc(inputs[0])
+        actual = sequence_obj.pattern_count(inputs[1])
+        self.assertEqual(expected, actual)
+
+    def test_pattern_count_test_one(self):
+        """
+        Test the pattern_count method using the TEST DATASET 1 fixture
+
+        Saved in PatternCountTest1.txt
+        This dataset just checks if you’re correctly counting.
+        It is the “easiest” test. Notice that all occurrences of CG in Text
+        (ACG​TACG​TACG​T) are away from the very edges (so your code won’t fail
+        on off­by­one errors at the beginning or at the end of Text) and that
+        none of the occurrences of Pattern overlap (so your code won’t fail if
+        you fail to account for overlaps).
+        """
+        fname = PATTERN_COUNT_TEST_FNAMES[0]
+        fpath = os.path.join(FIXTURE_PATH, fname)
+        file_util_obj = FileUtil(fpath)
+        inputs = file_util_obj.inputs
+        expected = int(file_util_obj.outputs[0])
+        sequence_obj = DnaFreqCalc(inputs[0])
+        actual = sequence_obj.pattern_count(inputs[1])
+        self.assertEqual(expected, actual)
+
+    def test_pattern_count_test_two(self):
+        """
+        Test the pattern_count method using the TEST DATASET 2 fixture
+
+        Saved in PatternCountTest2.txt
+        This dataset checks if your code correctly handles cases where there
+        is an occurrence of Pattern at the very beginning of Text.
+        Note that there are no overlapping occurrences of Pattern (i.e. AAAA),
+        and there is no occurrence of Pattern at the very end of Text,
+        so assuming your code passed Test Dataset 1, this test would only check
+        for off­by­one errors at the beginning of Text.
+        """
+        fname = PATTERN_COUNT_TEST_FNAMES[1]
+        fpath = os.path.join(FIXTURE_PATH, fname)
+        file_util_obj = FileUtil(fpath)
         inputs = file_util_obj.inputs
         expected = int(file_util_obj.outputs[0])
         sequence_obj = DnaFreqCalc(inputs[0])
